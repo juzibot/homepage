0704418d8183bd9b6c2de1ef9ce9393715d3ea95
Meeken1998 <meeken1998@gmail.com>
2021-12-12 12:37:28 +0800

    feat: update words

--- src/components/index/AppealBar/index.tsx
+++ src/components/index/AppealBar/index.tsx
@@ -10,7 +10,7 @@ const AppealBar: NextPage = () => {
   }, []);
   return (
     <div className="content" data-aos="fade-in">
-      <div className="title">现在开始，建立以社交关系为核心的营销体系</div>
+      <div className="title">即日起，建立以社交关系为核心的营销体系</div>
       <button className="white-button start-button">免费使用</button>
     </div>
   );
