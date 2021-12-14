bc399d7a809738f97e728b88ea95979747cce868
stardust <aoyunyoung@gmail.com>
2021-12-14 21:48:50 +0800

    add ci

---
+++ script/build_docker_ci.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set +e
+
+imageName='juzi-official-website'
+ECR_URL=789252305933.dkr.ecr.cn-northwest-1.amazonaws.com.cn
+
+#npm version | head -1 | grep -E -o '[0-9]{1}.[0-9]{1,2}.[0-9]{1,3}' > ./dist/release.version
+
+# PACKAGE_VERSION from CI build job
+
+
+echo current package version: "$PACKAGE_VERSION"
+
+IMAGE_META="$( aws ecr describe-images --repository-name=$imageName --image-ids=imageTag=$PACKAGE_VERSION 2> /dev/null )"
+
+if [[ $? == 0 ]]
+then
+  echo "$imageName:$PACKAGE_VERSION found, skip build docker image"
+  exit 0
+else
+  echo "$imageName:$PACKAGE_VERSION can be created"
+fi
+
+
+cp Dockerfile script/Dockerfile
+
+echo docker build -t "$imageName":"$PACKAGE_VERSION" .
+docker build -t "$imageName":"$PACKAGE_VERSION" .
+
+rm script/Dockerfile
+
+aws ecr get-login-password | docker login --username AWS --password-stdin $ECR_URL
+
+
+echo docker tag "$imageName":"$PACKAGE_VERSION" $ECR_URL/"$imageName":"$PACKAGE_VERSION"
+docker tag "$imageName":"$PACKAGE_VERSION" $ECR_URL/"$imageName":"$PACKAGE_VERSION"
+
+echo docker push $ECR_URL/"$imageName":"$PACKAGE_VERSION"
+docker push $ECR_URL/"$imageName":"$PACKAGE_VERSION"
