b0bdbc527e427444b44338ef31c11f9a502eb844
Meeken1998 <meeken1998@gmail.com>
2021-12-19 19:10:50 +0800

    fix bugs

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  i18n
+  i18n,
+  async redirects() {
+    return [
+      {
+        source: '/join-us',
+        destination: 'https://k0auuqcihb.jobs.feishu.cn/611573',
+        permanent: true,
+      }
+    ]
+  }
 }
