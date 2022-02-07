df9c12d62068f0fe6571e72c625164e09c788525
Meeken1998 <meeken1998@gmail.com>
2022-02-07 13:12:03 +0800

    feat: use cdn

--- src/components/index/LogosWall/index.tsx
+++ src/components/index/LogosWall/index.tsx
@@ -5,31 +5,22 @@ import { useState } from 'react';
 const logos = Array(44)
   .fill(null)
   .map((_, index) => {
-    return `https://cdn-official-website.juzibot.com/images/index-logos/${index + 1}.png`;
+    return `https://cdn-official-website.juzibot.com/images/index-logos/${
+      index + 1
+    }.png`;
   });
-
-// const logoUrls = [logos.slice(0, 22), logos.slice(22)];
 
 const LogosWall: NextPage = () => {
   const { t } = useTranslation(['homepage']);
-  const [isAnimatePaused, toggleAnimatePaused] = useState(false);
   return (
     <>
       <h1 className="title">{t('logos-wall-title')}</h1>
       <div className="logo-wall">
         <div className="scroll-mask" />
-        <div
-          className="logos-scroll-bar"
-          style={{ animationPlayState: isAnimatePaused ? 'paused' : 'running' }}
-        >
+        <div className="logos-scroll-bar">
           {logos.map((item, index) => {
             return (
-              <div
-                className="logo"
-                key={`${index}`}
-                onMouseMove={() => toggleAnimatePaused(true)}
-                onMouseLeave={() => toggleAnimatePaused(false)}
-              >
+              <div className="logo" key={`${index}`}>
                 <div className="logo-item">
                   <img src={item} alt="logo" draggable="false" />
                 </div>
@@ -37,26 +28,6 @@ const LogosWall: NextPage = () => {
             );
           })}
         </div>
-
-        {/* <div
-          className="logos-scroll-bar"
-          style={{ animationPlayState: isAnimatePaused ? 'paused' : 'running' }}
-        >
-          {logoUrls[1].map((item, index) => {
-            return (
-              <div
-                className="logo"
-                key={`${index + 22}`}
-                onMouseMove={() => toggleAnimatePaused(true)}
-                onMouseLeave={() => toggleAnimatePaused(false)}
-              >
-                <div className="logo-item">
-                  <img src={item} alt="logo" draggable="false" />
-                </div>
-              </div>
-            );
-          })}
-        </div> */}
       </div>
     </>
   );
