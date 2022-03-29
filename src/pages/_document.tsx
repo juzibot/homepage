aa0223bafd3af107acac1b59a9969f330e418c1a
Meeken1998 <meeken1998@gmail.com>
2022-03-29 18:48:37 +0800

    feat: add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,7 @@ class JuziSiteDocument extends Document {
       <Html>
         <title>句子互动企微SCRM - 助力搭建安全稳定私域流量</title>
         <meta name="viewport" content="width=1440, initial-scale=1.0" />
+        <meta name="baidu-site-verification" content="code-a7uFgEHa8D" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <Head />
         <body>
