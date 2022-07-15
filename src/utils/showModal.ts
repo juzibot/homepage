91127d2b165294e78194fd668ba224912046c013
Meeken1998 <meeken1998@gmail.com>
2022-07-16 01:45:37 +0800

    feat: update usage button

--- src/utils/showModal.ts
+++ src/utils/showModal.ts
@@ -1,15 +1,16 @@
-import { useTranslation } from 'react-i18next';
+// import { useTranslation } from 'react-i18next';
 
 export function useShowModal() {
-  const { i18n } = useTranslation();
+  // const { i18n } = useTranslation();
   if (typeof document === 'undefined') return () => {};
   return () => {
-    if (i18n.language === 'zh') {
-      document
-        .getElementById('contact-modal')
-        ?.setAttribute('style', 'display: flex');
-    } else {
-      window.open('https://miaohui.juzibot.com/auth/login', '_blank');
-    }
+    // if (i18n.language === 'zh') {
+    //   document
+    //     .getElementById('contact-modal')
+    //     ?.setAttribute('style', 'display: flex');
+    // } else {
+    //   window.open('https://miaohui.juzibot.com/auth/login', '_blank');
+    // }
+    window.open('https://miaohui.juzibot.com/keep', '_blank');
   };
 }
