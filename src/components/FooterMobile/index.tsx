e326065ff5256140111ca529f148aa153f0bb086
iseei <qwertpiyi@gmail.com>
2023-02-13 10:58:21 +0800

    update agreement link

--- src/components/FooterMobile/index.tsx
+++ src/components/FooterMobile/index.tsx
@@ -98,7 +98,7 @@ const FooterMobile: NextPage = () => {
         },
         {
           title: t('footer-menu-4-4-title'),
-          url: 'https://qiwei.juzibot.com/agreement',
+          url: 'https://miaohui.juzibot.com/agreement',
         },
         {
           title: t('footer-menu-4-5-title'),
