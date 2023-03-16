75f7ffe4df4159a00dfb9e223a9e88320da7746e
iseei <qwertpiyi@gmail.com>
2023-03-16 10:20:24 +0800

    update logos (#132)

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,5 +9,5 @@ export const juziHelloMsg = process.env.JUZI_HELLO_MSG || '';
 export const feishuBotUrl = process.env.FEISHU_BOT_URL || '';
 export const juziApiUrl = process.env.JUZI_API_URL || '';
 
-export const indexLogos = [1, 2, 3, 45, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+export const indexLogos = [2, 3, 45, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
   .map(v => `https://cdn-official-website.juzibot.com/images/index-logos/${v}.png`);
