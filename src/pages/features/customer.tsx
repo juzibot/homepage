1ebca61ecae92fe6a9d9c79ff430a08d9a9acbc5
menglin <menglin@xsky.com>
2023-06-03 15:15:18 +0800

    save

--- src/pages/features/customer.tsx
+++ src/pages/features/customer.tsx
@@ -8,14 +8,14 @@ import { useState } from 'react';
 const CustomerAcquisitionPage: NextPage = () => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <div className='m-auto'>
+    <div className='m-auto relative'>
       <ContactUsSimpleModal
         open={showModal}
         onCancel={() => setShowModal(false)}
       />
       <div
         onClick={() => setShowModal(true)}
-        className='w-[352px] h-[106px] rounded-full flex justify-center items-center text-[18px] font-semibold text-white absolute top-[315px] left-[50%] cursor-pointer'
+        className='w-[calc(15%)] h-[calc(8.25%)] rounded-full flex justify-center items-center text-[18px] font-semibold text-white absolute top-[39.7%] left-[50%] cursor-pointer'
         style={{ transform: 'translate(-50%)' }}
       ></div>
       <img className='w-full' alt='' src='/_images/image-page/customer-0.svg' />
