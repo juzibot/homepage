6a46ddb5fa13f8fe79ea1bb4cab99ca9a9ebb53b
hanyachao <hanyachao@juzibot.com>
2024-03-15 15:03:21 +0800

    rm tailwind-merge

--- src/components/PureModal/index.tsx
+++ src/components/PureModal/index.tsx
@@ -1,12 +1,12 @@
 import { Modal, ModalFuncProps } from "antd";
 import styles from './index.module.scss';
-import { twMerge } from 'tailwind-merge';
+import cls from 'classnames';
 
 export const PureModalInfoNoContent = (props: ModalFuncProps) => {
   return Modal.info({
     ...props,
     centered: props.centered ?? true,
     closable: props.closable ?? true,
-    className: twMerge(styles.PureModalInfoNoContent, props.className),
+    className: cls(styles.PureModalInfoNoContent, props.className),
   })
 }
